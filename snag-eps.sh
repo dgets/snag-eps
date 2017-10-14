1456de163f3806c9b7d3274879f7fc0fef459c24
Damon Getsman <damo.gets@gmail.com>
2017-10-14 09:50:57 -0500

    Working on #2; probably somewhere about midway.  Still have the sed work to do.

--- snag-eps.sh
+++ snag-eps.sh
@@ -12,11 +12,31 @@
 init() {
 	TRUE=1
 	FALSE=0
-	DEBUGGING=$FALSE
+	
+	PRIMITIVE=0
+	AUTOSPLIT=1
+
+	DEBUGGING=$TRUE
+	#VERBOSE=$FALSE
 	WGET=$(which wget)
 
+	return
+}
+
+init0() {
+	#used for 'primitive' mode (no autosplit)
 	urlString1=$1
 	urlString2=$2
+	min=$3
+	max=$4
+
+	return
+}
+
+init1() {
+	#autosplit mode
+	urlString=$1
+	delimitChar=$2
 	min=$3
 	max=$4
 
@@ -29,7 +49,14 @@ usage() {
 	printf "\t\tConcats urlString1 + (min <= ep# <=max) + urlString2\n"
 	printf "\t\tfor wgetting each successive URL; if the min value\n"
 	printf "\t\tcontains a preceding '0', all single digit numbers will\n"
-	printf "\t\tfollow suit.\n"
+	printf "\t\tfollow suit.\n\n"
+
+	if [ $DEBUGGING -eq $TRUE ] ; then
+		printf "Debugging:\n"
+		printf "Usage:\t%s -n urlString delimiter min max\n" "$0"
+		printf "\t\tNew usage; splits urlString using delimiter & "
+		printf "min.\n"
+	fi
 
 	exit 1
 }
@@ -51,15 +78,55 @@ determine_numbering_scheme() {
 	return "$prep0"
 }
 
-#main script entry
-init "$1" "$2" "$3" "$4"
+determine_mode() {
+	#NOTE: whilst determining the mode that we're running, this function
+	#also executes 'usage' for any invocation errors
 
-if [ $# -ne 4 ] ; then
-	usage
-else
-	#this line makes me think it's time to add a verbose flag :P
-	echo -n "$1 - $2 "
-	echo "- $3 - $4"
+	if [ $# -ne 4 ] ; then
+		if [ $DEBUGGING -eq $TRUE ] ; then
+			if [ $# -eq 5 ] ; then
+				if [ $1 != "-n" ] ; then
+					usage
+				else
+					mode=$AUTOSPLIT
+					init1
+				fi
+			else
+				usage
+			fi
+		else
+			usage
+		fi
+	else
+		#this line makes me think it's time to add a verbose flag :P
+		mode=$PRIMITIVE
+		#init0
+
+		if [ $DEBUGGING -eq $TRUE ] ; then
+			echo -n "$1 - $2 "
+			echo "- $3 - $4"
+		fi
+	fi
+
+	return $mode
+}
+
+#main script entry
+init 	#"$1" "$2" "$3" "$4"	#previous combined init()
+determine_mode
+
+if [ $? -eq $PRIMITIVE ] ; then
+	if [ $DEBUGGING -eq $TRUE ] ; then
+		echo "Primitive mode"
+	fi
+
+	init0 "$1" "$2" "$3" "$4"
+else	#autosplit
+	if [ $DEBUGGING -eq $TRUE ] ; then
+		echo "Autosplit mode"
+	fi
+
+	init1 "$2" "$3" "$4" "$5"	#first is the flag, ouah
 fi
 
 determine_numbering_scheme
